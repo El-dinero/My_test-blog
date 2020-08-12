fb449f0bf1a87cbe4f92ca6b1d0d498ba115eda1
El-dinero <mironenkovasila@gmail.com>
2020-08-12 20:09:19 +0300

    Test Post

--- routes/blog/posts.js
+++ routes/blog/posts.js
@@ -8,7 +8,29 @@ router.get("/", async (req, res) => {
     const post = await modelsPost.find({}).sort({ createdAt: -1 });
     res.status(200).json(post);
   } catch (error) {
-    console.error(error);
+    console.error("Error:", error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { title, body } = req.body;
+  try {
+    if (!title || !body) {
+      res.status(400).json({ msg: "Всі поля мають бути заповнені!" });
+    } else if (title.lenght < 30) {
+      res
+        .status(400)
+        .json({ msg: "Длина должна title бить не мениє 30 символов!" });
+    } else {
+      const newItem = await new modelsPost({
+        title,
+        body,
+      });
+      const post = await newItem.save();
+      res.status(200).json(post);
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
 
