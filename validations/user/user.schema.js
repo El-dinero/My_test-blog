6007a11d24809268f299232ebb31801d12eabd96
El-dinero <mironenkovasila@gmail.com>
2020-08-25 15:15:57 +0300

    Auth + midelvear

--- validations/user/user.schema.js
+++ validations/user/user.schema.js
@@ -1,16 +1,24 @@
 const joi = require("joi");
 const schema = {
   registerUser: joi.object({
-    loginUser: joi.string().max(50).message("Бrла").required(),
+    loginUser: joi
+      .string()
+      .min(3)
+      .max(50)
+      .message("Видіть емеїл від 3 до 50 символів")
+      .required(),
     emailUser: joi
       .string()
       .email({ tlds: { allow: true } })
       .required(),
     passwordUser: joi
       .string()
+      .min(6)
+      .max(40)
       .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
       .message("Блaа")
       .required(),
+    repeatPasswordUser: joi.valid(joi.ref("passwordUser")),
   }),
   authUser: joi.object({
     emailUser: joi
